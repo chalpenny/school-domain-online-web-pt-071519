78cba3a637770d11060357a8a892fb095c5f5563
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-04 02:37:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+
+class School
+
+   attr_reader :name #why isn't grade here?
+
+  def initialize(name) #why isn't grade here?
+    @name = name
+    @roster = {}
+  end
+
+  def roster #why do you need a reader method for this?
+    @roster
+  end
+
+
+  def add_student(name, grade)
+    if
+      @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+#  def add_student(name, grade)
+#    @roster[grade] = []
+#    @roster[grade] << name
+#  end
+
+end
